f8648b18d7332d8ae4c07be7fdf53f437ffb8fb7
Andrew McLean <mclean14@gmail.com>
2024-12-15 21:15:01 -0700

    Singly linked list implementation and reverse.

---
+++ Sources/DSAFun/LinkedList.swift
@@ -0,0 +1,188 @@
+//
+//  LinkedList.swift
+//  DSAFun
+//
+//  Created by Andrew McLean on 12/15/24.
+//
+
+import Foundation
+
+public class Node<T : Equatable> : CustomStringConvertible {
+	let value: T
+	var next: Node<T>?
+
+	init(value: T) {
+		self.value = value
+	}
+
+	public var description: String {
+		return "Node<\(T.self)>(\(self.value))"
+	}
+}
+
+public class LinkedList<T : Equatable> {
+	var head: Node<T>?
+
+	init(headValue: T? = nil) {
+		if let headValue {
+			self.head = .init(value: headValue)
+		}
+	}
+
+	init(values: [T]) {
+		values.forEach { value in
+			append(value)
+		}
+	}
+	
+	func getValues() -> [T] {
+		if head == nil { return [] }
+
+		var values: [T] = []
+		var current = head
+
+		while let node = current {
+			values.append(node.value)
+			current = node.next
+		}
+
+		return values
+	}
+
+	func length() -> Int {
+		if head == nil { return 0 }
+
+		var length: Int = 0
+		var current: Node<T>? = head
+
+		while let node = current {
+			length += 1
+			current = node.next
+		}
+
+		return length
+	}
+	
+	func append(_ value: T) {
+		guard let head else {
+			head = Node(value: value)
+			return
+		}
+
+		var current = head
+		while let node = current.next {
+			current = node
+		}
+
+		current.next = Node(value: value)
+	}
+
+	func prepend(_ value: T) {
+		// Create a new node
+		let newNode = Node(value: value)
+
+		// Set the newNode's next to the current head
+		// This is fine if head nil, just means the list is empty
+		newNode.next = head
+
+		// Set head to the new node so its at the front.
+		head = newNode
+	}
+
+	func insert(_ value: T, atIndex index: Int) -> Bool {
+		if index == 0 {
+			prepend(value)
+			return true
+		}
+
+		if index >= length() {
+			return false
+		}
+
+		let newNode = Node(value: value)
+		var current = head
+
+		for _ in 0..<index - 1 {
+			current = current?.next
+		}
+
+		newNode.next = current?.next
+		current?.next = newNode
+
+		return true
+	}
+
+	func removeFirst() {
+		head = head?.next
+	}
+
+	func removeLast() {
+		// If head is also last
+		if head?.next == nil {
+			head = nil
+		}
+
+		var previousNode: Node<T>? = nil
+		var nextNode = head?.next
+
+		while let node = nextNode?.next {
+			previousNode = nextNode
+			nextNode = node
+		}
+
+		previousNode?.next = nil
+	}
+
+	func remove(at index: Int) {
+		if index == 0 {
+			removeFirst()
+			return
+		}
+
+		var previous: Node<T>? = nil
+		var current = head
+		for _ in 0..<index {
+			previous = current
+			current = current?.next
+		}
+
+		previous?.next = current?.next
+		current?.next = nil
+	}
+
+	func reverse() {
+		// Track what will be the prev node
+		var prev: Node<T>? = nil
+		var current = head
+
+		while let node = current {
+			// Keep a reference to the next node in the list before mutating the node.next
+			let nextNode = node.next
+
+			// Swap the current node's next with the prev value
+			node.next = prev
+
+			// Set the prev value to current node
+			prev = node
+
+			// Just like we normally do, continue the loop
+			current = nextNode
+		}
+
+		head = prev
+	}
+}
+
+extension LinkedList : CustomStringConvertible {
+
+	public var description: String {
+		var values: [String] = []
+		var current = head
+		while let node = current {
+			values.append("\(node.value)")
+			current = node.next
+		}
+		return values.joined(separator: " -> ")
+	}
+
+}
---
+++ Tests/DSAFunTests/LinkedListTests.swift
@@ -0,0 +1,144 @@
+//
+//  LinkedListTests.swift
+//  DSAFun
+//
+//  Created by Andrew McLean on 12/15/24.
+//
+
+import XCTest
+@testable import DSAFun
+
+final class LinkedListTests: XCTestCase {
+
+    func testInitializersAndLength() throws {
+		let emptyList: LinkedList<Int> = .init()
+		XCTAssertNil(emptyList.head)
+		XCTAssertEqual(emptyList.length(), 0)
+
+		let listWithValues: LinkedList<Int> = .init(values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+		XCTAssertNotNil(listWithValues.head)
+		XCTAssertEqual(listWithValues.length(), 3)
+		print(listWithValues.description)
+    }
+
+	func testAppend() throws {
+		let list: LinkedList<Int> = .init(headValue: 1)
+		XCTAssertEqual(list.length(), 1)
+
+		list.append(2)
+		XCTAssertEqual(list.length(), 2)
+
+		list.append(3)
+		XCTAssertEqual(list.length(), 3)
+
+		XCTAssertEqual(list.getValues(), [1, 2, 3])
+	}
+
+	func testPrependEmpty() throws {
+		let list: LinkedList<Int> = .init()
+
+		list.prepend(1)
+
+		XCTAssertEqual(list.length(), 1)
+		XCTAssertEqual(list.head?.value, 1)
+	}
+
+	func testPrependNonEmpty() throws {
+		let list: LinkedList<Int> = .init(values: [2, 3, 4])
+
+		list.prepend(1)
+
+		XCTAssertEqual(list.length(), 4)
+		XCTAssertEqual(list.head?.value, 1)
+		XCTAssertEqual(list.getValues(), [1, 2, 3, 4])
+	}
+
+	func testInsertEmpty() throws {
+		let list: LinkedList<Int> = .init()
+
+		XCTAssertTrue(list.insert(1, atIndex: 0))
+		XCTAssertEqual(list.length(), 1)
+		XCTAssertEqual(list.head?.value, 1)
+	}
+
+	func testInsertMiddle() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 4, 5])
+
+		XCTAssertTrue(list.insert(3, atIndex: 2))
+		XCTAssertEqual(list.getValues(), [1, 2, 3, 4, 5])
+	}
+
+	func testInsertEnd() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 5])
+
+		XCTAssertTrue(list.insert(4, atIndex: 3))
+		XCTAssertEqual(list.getValues(), [1, 2, 3, 4, 5])
+	}
+
+	func testInsertOutOfBounds() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 4])
+
+		XCTAssertFalse(list.insert(4, atIndex: 4))
+		XCTAssertEqual(list.getValues(), [1, 2, 3, 4])
+	}
+
+	func testRemoveFirst() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 4, 5])
+
+		list.removeFirst()
+
+		XCTAssertEqual(list.getValues(), [2, 3, 4, 5])
+	}
+
+	func testRemoveFirstIndex() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 4, 5])
+
+		list.remove(at: 0)
+
+		XCTAssertEqual(list.getValues(), [2, 3, 4, 5])
+	}
+
+	func testRemoveLast() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 4, 5])
+
+		list.removeLast()
+
+		XCTAssertEqual(list.getValues(), [1, 2, 3, 4])
+	}
+
+	func testRemoveAt() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 4, 5])
+
+		list.remove(at: 2)
+
+		XCTAssertEqual(list.getValues(), [1, 2, 4, 5])
+	}
+
+	func testRemoveLastIndex() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 4, 5])
+
+		list.remove(at: 4)
+
+		XCTAssertEqual(list.getValues(), [1, 2, 3, 4])
+	}
+
+	func testRemoveOutOfBounds() throws {
+		let list: LinkedList<Int> = .init(values: [1, 2, 3, 4, 5])
+
+		list.remove(at: 8)
+
+		XCTAssertEqual(list.getValues(), [1, 2, 3, 4, 5])
+	}
+
+	func testReverse() {
+		let values: [Int] = [1, 2, 3, 4, 5]
+		let reversedValues: [Int] = [5, 4, 3, 2, 1]
+
+		let list: LinkedList<Int> = .init(values: values)
+
+		list.reverse()
+
+		XCTAssertEqual(list.getValues(), reversedValues)
+	}
+
+}
